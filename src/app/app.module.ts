11be682ff5539a1cdd076fc3b33c67f6c444afbf
rhotep <andreas.pittrich@posteo.de>
2021-12-30 22:07:55 +0100

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import	{	NgModule                    } 	from '@angular/core'
+import	{
+			IonicBaseAppModule,
+			IonicBaseAppComponent
+		} 									from '@rcc/ionic'
+
+@NgModule({
+	imports: [
+		IonicBaseAppModule
+	],
+	bootstrap: [IonicBaseAppComponent]
+})
+export class AppModule { }
